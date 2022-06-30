5151b1e0fda3e01dfb0c2d042b9d89c8dc436ccc
highballl <jyp0293@gmail.com>
2022-06-30 23:08:49 +0900

    2022/06/30/THU update test code: test suite must contain at least one test(2)

--- test/index.spec.js
+++ test/index.spec.js
@@ -8,4 +8,9 @@
 //   })
 // })
 
-describe('', () => {})
+describe('index', () => {
+  test('is a Vue instance', () => {
+    const wrapper = mount(index)
+    expect(wrapper.vm).toBeTruthy()
+  })
+})
