8336a590e1ef20b978413146c5df77454976fea8
highballl <jyp0293@gmail.com>
2022-06-30 22:27:32 +0900

    2022/06/30/THU Project realdew initial setting

---
+++ layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <Nuxt />
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'DefaultLayout',
+  data() {
+    return {}
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.wrap_app {
+  background-color: #121212;
+  width: 100%;
+  height: 100%;
+}
+</style>
